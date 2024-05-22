d3a70d81367850bd740248c65986ccc4b12f3aed
SABALBALJaoudat <72742901+SABALBALJaoudat@users.noreply.github.com>
2024-05-22 15:46:45 +0000

    Utilisation de dnd-kit

--- cardGame_frontend/src/App.tsx
+++ cardGame_frontend/src/App.tsx
@@ -1,98 +1,36 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
-import { Draggable, Droppable } from '@shopify/draggable';
-import { Point, motion } from "framer-motion"
+import "./App.css";
+import React, { useState } from 'react';
+import {DndContext} from '@dnd-kit/core';
 
-function App() {
-  // const containerRef = useRef(null);
-  // const dropzoneRef = useRef(null);
-  // const constraintsRef = useRef(null);
+import {Draggable} from './Draggable';
+import {Droppable} from './Droppable';
 
-  // useEffect(() => {
-  //   if (containerRef.current && dropzoneRef.current) {
-  //     const containers = [containerRef.current, dropzoneRef.current];
-
-  //     const droppable_monsters = new Droppable(containers, {
-  //       draggable: '.draggable-item_monsters',
-  //       dropzone: '.dropzone',
-  //     });
-
-  //     droppable_monsters.on('drag:start', (event) => {
-  //       console.log('Card dropped start');
-  //     });
-
-  //     droppable_monsters.on('droppable:dropped', (evt) => {
-  //       console.log('Card dropped in dropzone');
-  //     });
-
-  //     return () => {
-  //       droppable_monsters.destroy();
-  //     };
-  //   }
-  // }, []);
-
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const dropzoneRef = useRef<HTMLDivElement>(null);
-  const constraintsRef = useRef<HTMLDivElement>(null);
-
-  const checkIfInDropzone = (point: Point) => {
-    const dropzone = dropzoneRef.current?.getBoundingClientRect();
-    return (
-      dropzone &&
-      point.x >= dropzone.left &&
-      point.x <= dropzone.right &&
-      point.y >= dropzone.top &&
-      point.y <= dropzone.bottom
-    );
-  };
-
-  const adjustPosition = (x: number, y: number) => {
-    return { x: x - 50, y: y - 50 };
-  }
+export default function App() {
+  const containers = ['A', 'B', 'C'];
+  const [parent, setParent] = useState(null);
+  const draggableMarkup = (
+    <Draggable id="draggable">Drag me</Draggable>
+  );
 
   return (
-    <>
-      {/* <div ref={containerRef} className="cards-container dropzone">
-        <div className="draggable-item_monsters">Card 1</div>
-        <div className="draggable-item_monsters">Card 2</div>
-        <div className="draggable-item_spells">Card 3</div>
-      </div>
-      <div ref={dropzoneRef} className="dropzone">
-        Dropzone
-      </div> */}
-      <motion.div className="drag-area" ref={constraintsRef}>
-      <motion.div
-        className='drag-element'
-        drag
-        dragConstraints={constraintsRef}
-        dragMomentum={false}
-        animate={{ x: position.x, y: position.y }}
-        onDragStart={(event, info) => {
-          // setPosition({ x: info.point.x, y: info.point.y });
-        }}
-        onDragEnd={(event, info) => {
-          const adjustedPoint = adjustPosition(info.point.x, info.point.y);
-          setPosition({ x: adjustedPoint.x, y: adjustedPoint.y });
-          console.log(info.point.x, info.point.y);
-          // Check if the card is in the dropzone
-          if (!checkIfInDropzone(info.point)) {
-            // If not, reset to the initial position
-            setPosition({ x: 0, y: 0 });
-            console.log('Card dropped outside dropzone : ' + position.x + ' ' + position.y);
-          } else {
-            // Otherwise, update the position to the current position
-            console.log('Card dropped inside dropzone : ' + adjustedPoint.x + ' ' + adjustedPoint.y);
-          }
-        }}
-      >
-        Card 1
-      </motion.div>
-      <div ref={dropzoneRef} className='dropzone'>
-        Dropzone
-      </div>
-    </motion.div>
-    </>
-  )
-}
+    <DndContext onDragEnd={handleDragEnd}>
+      {parent === null ? draggableMarkup : null}
 
-export default App
+      {containers.map((id) => (
+        // We updated the Droppable component so it would accept an `id`
+        // prop and pass it to `useDroppable`
+        <Droppable key={id} id={id}>
+          {parent === id ? draggableMarkup : 'Drop here'}
+        </Droppable>
+      ))}
+    </DndContext>
+  );
+
+  function handleDragEnd(event: any) {
+    const {over} = event;
+
+    // If the item is dropped over a container, set it as the parent
+    // otherwise reset the parent to `null`
+    setParent(over ? over.id : null);
+  }
+};
