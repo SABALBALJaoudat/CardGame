f417de6cdd3577d5c55fef507eb8a5e4ebdf6723
SABALBALJaoudat <72742901+SABALBALJaoudat@users.noreply.github.com>
2024-05-24 15:25:54 +0000

    Ajout d'un deuxieme element à drag and drop distinct + nouvelle dropzone distinct

--- cardGame_frontend/src/App.tsx
+++ cardGame_frontend/src/App.tsx
@@ -5,29 +5,46 @@ import { Draggable } from './Draggable';
 import { Droppable } from './Droppable';
 
 export default function App() {
-  const containers = ['A', 'B', 'C'];
-  const [parent, setParent] = useState<string | null>(null);
+  const containers_monsters = ['zone_monsters_A', 'zone_monsters_B', 'zone_monsters_C'];
+  const containers_magictrap = ['zone_magictrap_A', 'zone_magictrap_B', 'zone_magictrap_C'];
+  const [parent, setParent] = useState<{ [key: string]: string | null }>({
+    draggable_monster: null,
+    draggable_magic: null
+  });
 
-  const handleDragEnd = ({ over }: { over: any }) => {
-    console.log(over);
+  const handleDragEnd = ({ active, over }: { active: any, over: any }) => {
     if (over) {
-      setParent(over.id);
-    }
-    else {
-      setParent(null);
+      setParent((prev) => ({
+        ...prev,
+        [active.id]: over.id
+      }));
+    } else {
+      setParent((prev) => ({
+        ...prev,
+        [active.id]: null
+      }));
     }
   };
 
-  const draggableMarkup = (
-    <Draggable id="draggable">Drag me</Draggable>
+  const draggableMarkup_monsters = (
+    <Draggable id="draggable_monster">Monster</Draggable>
+  );
+  const draggableMarkup_magic = (
+    <Draggable id="draggable_magic">Magic/Trap</Draggable>
   );
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      {parent === null ? draggableMarkup : null}
-      {containers.map((id) => (
+      {parent.draggable_monster === null ? draggableMarkup_monsters : null}
+      {parent.draggable_magic === null ? draggableMarkup_magic : null}
+      {containers_monsters.map((id) => (
         <Droppable key={id} id={id}>
-          {parent === id ? <Draggable id="draggable">Drag me</Draggable> : 'Drop here'}
+          {parent.draggable_monster === id ? draggableMarkup_monsters : 'Drop here'}
+        </Droppable>
+      ))}
+      {containers_magictrap.map((id) => (
+        <Droppable key={id} id={id}>
+          {parent.draggable_magic === id ? draggableMarkup_magic : 'Drop here'}
         </Droppable>
       ))}
     </DndContext>
