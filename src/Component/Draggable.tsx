f0b3ae08bf63ef75cfc8f8c3bbcc7d02af788b48
SABALBALJaoudat <72742901+SABALBALJaoudat@users.noreply.github.com>
2024-07-17 15:41:55 +0000

    1er essai de mouvement 3D lors d'un Drag

--- src/Component/Draggable.tsx
+++ src/Component/Draggable.tsx
@@ -1,6 +1,7 @@
 import { useDraggable } from '@dnd-kit/core';
 import { useDispatch } from 'react-redux';
 import { setHoveredCardInfo } from '../Store/draggableSlice';
+import { useState } from 'react';
 
 interface DraggableProps {
   monster: Monster;
@@ -10,11 +11,13 @@ export function Draggable({ monster }: DraggableProps) {
   const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
     id: monster.id,
   });
+  const [rotation, setRotation] = useState({ x: 0, y: 0 });
+  const [prevMousePos, setPrevMousePos] = useState({ x: 0, y: 0 });
 
   const dispatch = useDispatch();
 
   const containerStyle = {
-    transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : '',
+    transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0) rotateY(${rotation.y}deg) rotateX(${rotation.x}deg)` : '',
     cursor: 'grab',
   };
 
@@ -28,13 +31,42 @@ export function Draggable({ monster }: DraggableProps) {
     transition: 'transform 0.2s ease',
   };
 
-  const handleMouseEnter = () => {
-    console.log(monster.id);
+  const handleMouseMove = (e: { clientX: number; clientY: number; }) => {
+    const deltaX = e.clientX - prevMousePos.x;
+    const deltaY = e.clientY - prevMousePos.y;
+
+    setRotation((prevRotation) => ({
+      x: prevRotation.x + deltaY * 0.1,
+      y: prevRotation.y + deltaX * 0.1,
+    }));
+
+    setPrevMousePos({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseLeave = () => {
+    setRotation({ x: 0, y: 0 });
+  };
+
+  // useEffect(() => {
+  //   const handleDragStart = () => setIsDraggingNow(true);
+  //   const handleDragEnd = () => setIsDraggingNow(false);
+
+  //   window.addEventListener('dragstart', handleDragStart);
+  //   window.addEventListener('dragend', handleDragEnd);
+
+  //   return () => {
+  //     window.removeEventListener('dragstart', handleDragStart);
+  //     window.removeEventListener('dragend', handleDragEnd);
+  //   };
+  // }, []);
+
+  const handleMouseEnter = (e: { clientX: any; clientY: any; }) => {
+    setPrevMousePos({ x: e.clientX, y: e.clientY });
     dispatch(setHoveredCardInfo(monster));
   };
 
   return (
-    <div ref={setNodeRef} style={containerStyle} {...listeners} {...attributes} onMouseEnter={handleMouseEnter}>
+    <div ref={setNodeRef} style={containerStyle} {...listeners} {...attributes} onMouseEnter={handleMouseEnter} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
       <div style={innerStyle}>
         {monster.title}
       </div>
